d02007bc795f879903d12a0135873d562b83be5d
edbjunwang <junwang@enterprisedb.com>
2025-01-13 17:12:47 +0800

    test: code style

--- pycode/main.py
+++ pycode/main.py
@@ -3,9 +3,14 @@
 num1 = 1.5
 num2 = 6.3
 
+# make a very long line to check if pycodestype can detect that... blah blaaaahhh blaahhhh blaaaaaaaaaaaaahhhhhhh blaaaaaaaaaaaahhhh blahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+
 # Add two numbers
 sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+if sum: 
+    print('The sum is not zeor\n')
 
